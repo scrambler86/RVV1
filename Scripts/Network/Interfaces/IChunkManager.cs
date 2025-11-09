6cb64adb07fe7b97a533d351d91af1c39c9ea10d
scrambler86 <106551051+scrambler86@users.noreply.github.com>
2025-11-09 18:20:20 +0100

    Delete Scripts directory

--- Scripts/Network/Interfaces/IChunkManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using Game.Networking.Adapters;
-
-public interface IChunkManager
-{
-    void RegisterPlayer(IPlayerNetworkDriver drv);
-    void UnregisterPlayer(IPlayerNetworkDriver drv);
-    void UpdatePlayerChunk(IPlayerNetworkDriver drv, Vector3 worldPos);
-}
